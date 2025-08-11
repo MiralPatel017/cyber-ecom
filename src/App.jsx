a16e4299534f2101f74b350ebeacba641a4f4102
miralizion2024-netizen <miralkamleshbhai@gmail.com>
2025-08-11 22:17:59 +0530

    add Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/LoginFolder/LoginPage'
 
 function App() {
 
